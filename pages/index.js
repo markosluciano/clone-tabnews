3706c7447b6b647423586c83f138ec4fd803ba2c
Marcos Luciano <153950095+markosluciano@users.noreply.github.com>
2025-02-25 13:52:41 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
-function Home (){
-        return <h1>Página inicial do projeto Tabnews do curso Dev.com!</h1>
+function Home() {
+  return <h1>Página inicial do projeto Tabnews do curso Dev.com!</h1>;
 }
+function teste() {
+  console.log("teste");
+}
+function teste() {
+  console.log("teste de identação");
+}
+
 export default Home;
