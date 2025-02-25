f5c7324bc801cfe95b7ec737bcb655a48c213ff2
Marcos Luciano <153950095+markosluciano@users.noreply.github.com>
2025-02-25 16:12:48 +0000

    `adiciona o prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
 function Home() {
   return <h1>Página inicial do projeto Tabnews do curso Dev.com!</h1>;
 }
-function teste() {
-  console.log("teste");
-}
-function teste() {
-  console.log("teste de identação");
-}
-
 export default Home;
